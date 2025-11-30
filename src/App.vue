ea2608e79d0fc8adb4f4bd2126a758c5a200fe82
Hai <sunbaoqi08@gmail.com>
2025-12-01 07:57:25 +0800

    到164了

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+// const goList = () => {
+//   this.$router.push('/list')
+// }
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter() //路由对象
+const route = useRoute() //路由参数
+
+const goList = () => {
+  router.push('/list')
+  console.log(router, route)
+}
+</script>
 
 <template>
-  <div>我是div</div>
+  <div>
+    我是App
+    <button @click="$router.push('/home')">跳首页</button>
+    <button @click="goList">跳列表页</button>
+  </div>
 </template>
 
 <style scoped></style>
