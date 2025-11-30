ce187e0c9bb828b9e63cb0e3e05461c90a1f8050
Hai <sunbaoqi08@gmail.com>
2025-12-01 07:01:14 +0800

    git提交前校验

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import pluginPrettier from 'eslint-plugin-prettier'
 export default defineConfig([
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    files: ['**/*.{js,mjs,jsx,vue}']
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
@@ -15,16 +15,16 @@ export default defineConfig([
   {
     languageOptions: {
       globals: {
-        ...globals.browser,
-      },
-    },
+        ...globals.browser
+      }
+    }
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   {
     plugins: {
-      prettier: pluginPrettier,
+      prettier: pluginPrettier
     },
     rules: {
       'prettier/prettier': [
@@ -34,18 +34,18 @@ export default defineConfig([
           semi: false, // 无分号
           printWidth: 80, // 每行宽度至多80字符
           trailingComma: 'none', // 不加对象|数组最后逗号
-          endOfLine: 'auto', // 换行符号不限制（win mac 不一致）
-        },
+          endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
+        }
       ],
       'vue/multi-word-component-names': [
         'warn',
         {
-          ignores: ['index'], // vue组件名称多单词组成（忽略index.vue）
-        },
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
       ],
       'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
       // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
-      'no-undef': 'error',
-    },
-  },
+      'no-undef': 'error'
+    }
+  }
 ])
