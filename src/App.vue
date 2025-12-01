f62b25d785da2813343b75493b54f5495db9af9f
Hai <sunbaoqi08@gmail.com>
2025-12-01 08:14:19 +0800

    element plus

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,11 @@ const goList = () => {
     我是App
     <el-button @click="$router.push('/home')">跳首页</el-button>
     <el-button @click="goList">跳列表页</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <p>{{ token }}</p>
+    <el-button>登录</el-button>
+    <el-button></el-button>
   </div>
 </template>
 
